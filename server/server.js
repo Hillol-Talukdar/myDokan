95b043d468eb15b45cbdec6fe092667b701bdeb2
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-08-07 15:50:15 +0600

    database connecting completed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require("./app");
+
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("DB CONNECTED"))
+    .catch((err) => console.log("DB CONNECTION ERROR", err));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`App running on port: ${port}`));
