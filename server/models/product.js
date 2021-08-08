45474a386b2419be7c3209bffef70262931bbf8d
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-08-08 10:25:43 +0600

    created model for order, product and user

---
+++ server/models/product.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const productSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        image: String,
+        brand: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        reviews: [reviewSchema],
+        rating: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        numReviews: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        price: {
+            type: number,
+            required: true,
+            deafult: 0,
+        },
+        countInStock: {
+            type: number,
+            required: true,
+            deafult: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Product", productSchema);
