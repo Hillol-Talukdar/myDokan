b28a4b0635df64dcf23b0040ea6e39915c1b3866
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-08-28 12:39:15 +0600

    implemented remove from cart fearture. BUG FIXED for add to cart items

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartitems: [] }, action) => {
     switch (action.type) {
@@ -12,13 +12,24 @@ export const cartReducer = (state = { cartitems: [] }, action) => {
             if (existItem) {
                 return {
                     ...state,
-                    cartItemms: state.cartItems.map((x) =>
+                    cartItems: state.cartItems.map((x) =>
                         x.product === existItem.product ? item : x
                     ),
                 };
             } else {
-                return { ...state, cartItems: [...state.cartItems, item] };
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item],
+                };
             }
+
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    (x) => x.product !== action.payload
+                ),
+            };
 
         default:
             return state;
