547c663752531a2f177a7ac87268704ac2ca49d2
Hillol Talukdar <talukdarhillol@gmail.com>
2021-10-16 18:05:18 +0600

    implemented login screen and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
     return (
@@ -13,13 +14,14 @@ const App = () => {
             <Header />
             <main className="py-3">
                 <Container>
+                    <Route path="/login" component={LoginScreen} exact />
                     <Route path="/" component={HomeScreen} exact />
                     <Route
                         path="/product/:id"
                         component={ProductScreen}
                         exact
                     />
-                    <Route path="/cart/:id?" component={CartScreen}/>
+                    <Route path="/cart/:id?" component={CartScreen} />
                 </Container>
             </main>
             <Footer />
--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -17,11 +17,14 @@ export const login = (email, password) => async (dispatch) => {
             },
         };
 
-        const { data } = await axios.post("/api/users/login", {
-            email,
-            password,
-            config,
-        });
+        const { data } = await axios.post(
+            "/api/users/login",
+            {
+                email,
+                password,
+            },
+            config
+        );
 
         dispatch({
             type: USER_LOGIN_SUCCESS,
