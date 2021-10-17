dcd1d302845f66bb745f02cebaf697fb0130fc30
Hillol Talukdar <talukdarhillol@gmail.com>
2021-10-17 14:57:36 +0600

    created profile screen and get user details feature

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -6,9 +6,13 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
 } from "../constants/userConstant";
 
-export const userLoginReducer = (state = { user: [] }, action) => {
+export const userLoginReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_LOGIN_REQUEST:
             return { loading: true };
@@ -23,7 +27,7 @@ export const userLoginReducer = (state = { user: [] }, action) => {
     }
 };
 
-export const userRegisterReducer = (state = { user: [] }, action) => {
+export const userRegisterReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_REGISTER_REQUEST:
             return { loading: true };
@@ -31,6 +35,21 @@ export const userRegisterReducer = (state = { user: [] }, action) => {
             return { loading: false, userInfo: action.payload };
         case USER_REGISTER_FAIL:
             return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true };
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload };
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        case USER_DETAILS_RESET:
+            return { user: {} };
         default:
             return state;
     }
