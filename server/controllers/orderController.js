32d341bae60f2e6cce27009480b140fe2eed31b3
Hillol Talukdar <talukdarhillol@gmail.com>
2021-10-18 14:45:47 +0600

    created endpoint for adding order

---
+++ server/controllers/orderController.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("express-async-handler");
+const Order = require("../models/order");
+
+exports.addOrderItems = asyncHandler(async (req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orederItems && orderItems.length == 0) {
+        res.status(400);
+        throw new Error("No order items");
+        return;
+    } else {
+        const order = new Order({
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
+});
