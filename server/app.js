b7391b8acebb0d820e368670ed90f049fdcf9ebe
Hillol Talukdar <talukdarhillol@gmail.com>
2021-11-02 10:57:00 +0600

    created config and enppoint for uploading image

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 
 const app = express();
 
@@ -19,6 +21,10 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 
